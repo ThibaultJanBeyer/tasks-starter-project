09d6ed2d21f7ee5032f89e3a119a425c2043fbee
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2022-01-31 11:24:45 +0100

    extract protocol

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 
 export const setupDb = async () => {
   // env variables in dev are set via config/default.json
-  const { host, port, user, password, db } = config.get('dbConfig')
-  const uri = `mongodb://${user}:${password}@${host}:${port}/${db}?retryWrites=true&w=majority`
+  const { protocol, host, port, user, password, db } = config.get('dbConfig')
+  const uri = `${protocol}${user}:${password}@${host}:${port}/${db}?retryWrites=true&w=majority`
   // console.log(`Connecting to ${uri}`) // for debugging
   await mongoose.connect(uri)
 }
