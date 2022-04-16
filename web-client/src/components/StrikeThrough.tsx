0a7c923d107d4796cd5987a4c1ee7bf537bd8977
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2022-04-16 22:48:03 +0200

    update webclient deps

--- web-client/src/components/StrikeThrough.tsx
+++ web-client/src/components/StrikeThrough.tsx
@@ -1,4 +1,5 @@
-export const StrikeThrough: React.FC<{ strike: boolean }> = ({
-  strike,
-  children,
-}) => (strike ? <s>{children}</s> : <>{children}</>)
+import React from 'react'
+
+export const StrikeThrough: React.FC<
+  React.PropsWithChildren<{ strike: boolean }>
+> = ({ strike, children }) => (strike ? <s>{children}</s> : <>{children}</>)
--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { Tasks } from './pages/tasks/Tasks'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
+
+root.render(
   <React.StrictMode>
     <Tasks />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
